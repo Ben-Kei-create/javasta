9692f4dfadd5913b0315b3afa07f8eacbc590d54
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-04-19 07:11:57 +0900

    Initial Commit

---
+++ Javasta/JavastaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JavastaApp.swift
+//  Javasta
+//
+//  Created by 茂木史明 on 2026/04/19.
+//
+
+import SwiftUI
+
+@main
+struct JavastaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
